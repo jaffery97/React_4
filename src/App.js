79b9a0c9762f65f349d63dcbff0e24d881d3abd7
MH Jafory <mhjafory@MHs-MacBook-Pro.local>
2020-03-03 20:26:13 -0500

    upadte

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+
+constructor(){
+  super();
+  this.state = {
+    items: [],
+    isloaded: false,
+  }
+}
+
+//this is where our methods live
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users').then(res => {
+    return res.json();
+  })
+
+  .then(data => {
+    console.log(data);
+    this.setState({
+      isloaded: true,
+      items: data,
+    });
+  });
+}
+
+
 render(){
+  //this is deconstructing the "this.state" object
+  var {isloaded, items} = this.state;
+  if (!isloaded) {
+    return <div>Loading....</div>
+
+  }
+  else {
   return (
-    <div></div>
+
+    <div ClassName="App">
+     <div ClassName="Names">
+         <ul>
+         {items.map(el => {
+           return (
+             <li key={el.id}>
+             Name: {el.name} | UserName: {el.username} | {''}
+             <a href={'https://${el.website}'}>Website</a>
+             </li>
+           );
+         })}
+         </ul>
+     </div>
+    </div>
   );
 };
 };
+}
 
 
 export default App;
